5314a8e20b67b0c4d634c2a914015636526266fe
UNILORN <b5159@oic.jp>
2016-10-14 00:33:48 +0900

    a

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 composer install
+composer update
 
 if [! -e .env]; then
 	cp .env.example .env
@@ -8,5 +9,14 @@ fi
 
 php artisan key:generate
 
-mysql -u root -p -e 'create database OIC_Book'
-mysql -u root -p OIC_Book << OIC_Book.sql
+echo "OIC_Book を一度作成しましたか？(Y/N)"
+read ans
+
+if [ $ans = N ]; then
+	mysql -u root -p -e 'create database OIC_Book'
+	mysql -u root -p OIC_Book < OIC_Book.sql
+else
+	mysql -u root -p OIC_Book < OIC_Book.sql
+fi
+
+
