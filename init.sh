94c659279185a16455cddfcb1f36d9eeb2564a78
UNILORN <b5159@oic.jp>
2016-11-07 22:47:26 +0900

    initsh 更新

--- init.sh
+++ init.sh
@@ -10,9 +10,15 @@ echo "OIC_Book を一度作成しましたか？(Y/N)"
 read ans
 
 if [ $ans = N ]; then
+    echo "Database[OIC_Book] Create Now!"
+    echo "mySQLのパスワードを入力"
 	mysql -u root -p -e 'create database OIC_Book'
+	echo "[OIC_Book] Dump file write"
+    echo "mySQLのパスワードを入力"
 	mysql -u root -p OIC_Book < OIC_Book.sql
 else
+    echo "[OIC_Book] Dump file write"
+    echo "mySQLのパスワードを入力"
 	mysql -u root -p OIC_Book < OIC_Book.sql
 fi
 
