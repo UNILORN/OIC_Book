5591937050424f58a3504bd5055a58c3ac711a2e
Serorin <b5239@oic.jp>
2016-11-08 14:46:51 +0900

    CART&REVIEW作成

---
+++ database/seeds/REVIEW_TABLE.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\REVIEW;
+class REVIEW_TABLE extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('REVIEW')->delete();
+
+       //faker使う。普通に使う場合と同じ。
+      $faker = Faker\Factory::create('ja_JP');
+
+      for($i=0; $i<100; $i++)
+      {
+          DB::table('REVIEW')->insert([
+            'product_id'=>$i,
+            'user_id'=>$i,
+            'review'=>$faker->randomDigitNotNull(),
+            'review_text'=>$faker->word(),
+            'entry_time'=>$faker->dateTimeThisCentury()
+          ]);
+      }
+    }
+}
