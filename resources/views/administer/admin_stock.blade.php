c687932cfe54339c19a44fe5ba53655a7d2416be
yuyutyanu <yuyutyanu@yahoo.co.jp>
2016-11-11 22:35:09 +0900

    検索追加削除機能のUI作成

--- resources/views/administer/admin_stock.blade.php
+++ resources/views/administer/admin_stock.blade.php
@@ -5,6 +5,33 @@
 
 @section('main')
 
+<div class="search">
+  <p>商品名</p>
+  <input type="text" name="some_name" value="">
+  <h2 class="close">閉じる</h2>
+</div>
+
+<div class="add">
+   <p>商品ID</p>
+   <input type="text" name="some_name" value="">
+   <p>商品名</p>
+   <input type="text" name="some_name" value="">
+   <p>在庫</p>
+   <input type="text" name="some_name" value="">
+   <p>単価</p>
+   <input type="text" name="some_name" value="">
+   <h2 class="close">閉じる</h2>
+</div>
+
+<div class="delete">
+  <p>商品ID</p>
+  <input type="text" name="some_name" value="">
+  <p>商品名</p>
+  <input type="text" name="some_name" value="">
+  <h2 class="close">閉じる</h2>
+</div>
+
+
 <div class="container">
   <div id="products_header">
     <li>商品ID</li>
@@ -23,12 +50,36 @@
   @endforeach
 
   <div id="product_edit">
-    <a href="#"><li>検索</li></a>
-    <a href="#"><li>追加</li></a>
-    <a href="#"><li>削除</li></a>
-    <a href="#"><li>変更</li></a>
+    <a href="#" class="search_product"><li>検索</li></a>
+    <a href="#" class="add_product"><li>追加</li></a>
+    <a href="#" class="delete_product"><li>削除</li></a>
   </div>
-
 </div>
+
+<script charset="utf-8">
+ $('.search_product').click(function(){
+   $('.delete').fadeOut();
+   $('.add').fadeOut();
+   $('.search').fadeIn();
+ })
+
+ $('.add_product').click(function(){
+   $('.delete').fadeOut();
+   $('.search').fadeOut();
+   $('.add').fadeIn();
+ })
+
+$('.delete_product').click(function(){
+  $('.search').fadeOut();
+  $('.add').fadeOut();
+  $('.delete').fadeIn();
+})
+
+$('.close').click(function(){
+  $('.search').fadeOut();
+  $('.add').fadeOut();
+  $('.delete').fadeOut();
+})
+</script>
 
 @endsection
