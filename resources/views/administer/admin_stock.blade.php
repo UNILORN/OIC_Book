d940481d234165c55103502d84f9eb6899078b1c
yuyutyanu <yuyutyanu@yahoo.co.jp>
2016-11-11 16:18:39 +0900

    管理者の商品情報編集ページ

--- resources/views/administer/admin_stock.blade.php
+++ resources/views/administer/admin_stock.blade.php
@@ -1,1 +1,31 @@
 @extends('/administer/admin_template')
+
+@section('css','/admin/stock')
+@section('title','stock')
+
+@section('main')
+
+<div class="container">
+  <div id="products_header">
+    <li>商品名</li>
+    <li>個数</li>
+    <li>単価</li>
+  </div>
+
+  @foreach ($products as $product)
+  <div class="product">
+      <li>{{$product->product_name}}</li>
+      <li>{{$product->product_stock}}個</li>
+      <li>{{$product->product_price}}円</li>
+  </div>
+  @endforeach
+
+  <div id="product_edit">
+    <a href="#"><li>商品追加</li></a>
+    <a href="#"><li>商品削除</li></a>
+    <a href="#"><li>単価変更</li></a>
+  </div>
+
+</div>
+
+@endsection
--- resources/views/administer/admin_template.blade.php
+++ resources/views/administer/admin_template.blade.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="/css/admin/admin_template.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="/css/reset.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="/css/@yield('css').css" media="screen" title="no title" charset="utf-8">
     <title>@yield('title')</title>
   </head>
   <body>
     <div class="sidebar">
-    <a href="/admin/stock"><li>在庫</li></a>
+    <a href="/admin/stock"><li>商品情報</li></a>
     <a href="/admin/employee"><li>従業員</li></a>
     <a href="/admin/sales"><li>売上</li></a>
     <a href="/admin/history"><li>履歴</li></a>
