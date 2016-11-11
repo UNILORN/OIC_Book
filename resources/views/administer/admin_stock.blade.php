356a4f9eea796ea5a6070d1e40a4964df2e861c7
yuyutyanu <yuyutyanu@yahoo.co.jp>
2016-11-11 21:00:03 +0900

    在庫確認＆商品情報編集ページ追加

--- resources/views/administer/admin_stock.blade.php
+++ resources/views/administer/admin_stock.blade.php
@@ -7,13 +7,15 @@
 
 <div class="container">
   <div id="products_header">
+    <li>商品ID</li>
     <li>商品名</li>
-    <li>個数</li>
+    <li>在庫</li>
     <li>単価</li>
   </div>
 
   @foreach ($products as $product)
   <div class="product">
+      <li>{{$product->product_id}}</li>
       <li>{{$product->product_name}}</li>
       <li>{{$product->product_stock}}個</li>
       <li>{{$product->product_price}}円</li>
@@ -21,9 +23,10 @@
   @endforeach
 
   <div id="product_edit">
-    <a href="#"><li>商品追加</li></a>
-    <a href="#"><li>商品削除</li></a>
-    <a href="#"><li>単価変更</li></a>
+    <a href="#"><li>検索</li></a>
+    <a href="#"><li>追加</li></a>
+    <a href="#"><li>削除</li></a>
+    <a href="#"><li>変更</li></a>
   </div>
 
 </div>
