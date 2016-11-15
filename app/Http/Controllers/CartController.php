2401da2431556fdb43b91e0b8aeac8c3251d364d
ninja250 <naitomea.mky@gmail.com>
2016-11-15 15:15:32 +0900

    cartController変更

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,4 +11,18 @@ class CartController extends Controller
   public function index(){
     return view('cart');
   }
+  public function show(Request $request){
+    $id = $request->get("id");
+    $id = DB::table('users')->insertGetId(
+    ['product_id' => '$id' ]
+    );
+    return redirect("/cart");
+  }
+
+  public function delete(Request $request){
+    $id = $request->get("id");
+    $product = DB::table('CART')->where('id',$id)->first();
+    $product = DB::table('CART')->where('id',$id)->delete();
+    return redirect("/cart");
+  }
 }
