c5892fa1512472fc6f684ba070e893c3af875ae3
UNILORN <b5159@oic.jp>
2016-11-21 21:59:32 +0900

    商品リスト更新

--- resources/views/administer/admin_stock.blade.php
+++ resources/views/administer/admin_stock.blade.php
@@ -4,5 +4,21 @@
 @section('title','stock')
 
 @section('main')
+<table class="table">
+  <tr>
+    <th>商品ID</th>
+    <th>商品名称</th>
+    <th>価格</th>
+    <th>在庫数（個）</th>
+  </tr>
+  @foreach ($products as $key => $value)
+    <tr>
+      <td>{{$value->product_id}}</td>
+      <td>{{$value->product_name}}</td>
+      <td>{{$value->product_price}}</td>
+      <td>{{$value->product_stock}}</td>
+    </tr>
+  @endforeach
 
+</table>
 @endsection
