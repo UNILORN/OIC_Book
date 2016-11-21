340ad4a92abcb9c5913293712713bd59ad421b04
UNILORN <b5159@oic.jp>
2016-11-21 19:18:54 +0900

    ちゃぬごめんなさい管理者ページやります。

--- resources/views/administer/admin_template.blade.php
+++ resources/views/administer/admin_template.blade.php
@@ -5,18 +5,14 @@
     <link rel="stylesheet" href="/css/admin/admin_template.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="/css/reset.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="/css/@yield('css').css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="/js/jquery.min.js" charset="utf-8"></script>
     <title>@yield('title')</title>
   </head>
   <body>
-    <div class="sidebar">
-    <a href="/admin/stock"><li>商品情報</li></a>
-    <a href="/admin/employee"><li>従業員</li></a>
-    <a href="/admin/uoderdetail"><li>売上</li></a>
-    <a href="/admin/order"><li>発注</li></a>
-    <a href="/admin/arrive"><li>入荷情報</li></a>
-    <a href="/admin/payment"><li>入金確認</li></a>
-    </div>
+
       @yield('main')
   </body>
 </html>
