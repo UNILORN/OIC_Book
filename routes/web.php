f5cacb4d33b16c27302efce6c64634fafc45b5e4
UNILORN <b5159@oic.jp>
2016-11-22 04:27:05 +0900

    Route修正

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,33 @@
 Auth::routes();
 
 Route::get('/', 'TopController@index');
-
+Route::get('/logout',function(){
+  Auth::logout();
+  return redirect('/');
+});
 
 //新規登録
 Route::resource('/signup','SignupController');
 Route::get('/signup_confirm','ConfirmController@signup');
+
 //商品
 Route::get('/product', 'ProductsController@index');
 Route::get('/detail','DetailController@index');
+
 //カート
 Route::resource('/cart', 'CartController');
+
+//マイページ
+Route::resource('/mypage','MypageController');
+
 //購入
 Route::get('/buy','BuyController@index');
 Route::get('/buy_confirm','ConfirmController@buy');
-//ログイン
-//Route::resource('/login','LoginController');
-//マイページ
-Route::resource('/mypage','MypageController');
+
 //退会処理
 Route::get('/recede','RecedeController@index');
 Route::get('/recede_confirm','ConfirmController@recode');
+
 
 //管理者ページ
 Route::get('/admin','AdmintopController@index');
@@ -45,10 +52,4 @@ Route::get('/admin/history','AdminhistoryController@index');
 Route::get('/admin/order','AdminorderController@index');
 Route::get('/admin/arrive','AdminarriveController@index');
 Route::get('/admin/payment','AdminpaymentController@index');
-
-//管理者用フォーム
 Route::get('/admin/payment_form','AdminpaymentController@submit');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
