b9c01ab0b2990aba171ebb7da34400813317b9e8
UNILORN <b5159@oic.jp>
2016-11-21 22:57:09 +0900

    Vendorのリレーション確定させるための変更

--- database/seeds/VENDOR_TABLE.php
+++ database/seeds/VENDOR_TABLE.php
@@ -16,9 +16,10 @@ class VENDOR_TABLE extends Seeder
        //faker使う。普通に使う場合と同じ。
       $faker = Faker\Factory::create('ja_JP');
 
-      for($i=0; $i<100; $i++)
+      for($i=1; $i<=100; $i++)
       {
           DB::table('VENDOR')->insert([
+            'vendor_id'=>$i,
             'vendor_name'=>$faker->company(),
             'vendor_email'=>$faker->email(),
             'vendor_address'=>$faker->address(),
