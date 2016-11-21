c2b731d755cca4451e8c7a1f3ab559867463c70e
UNILORN <b5159@oic.jp>
2016-11-21 22:00:12 +0900

    従業員Blade更新

--- resources/views/administer/admin_employee.blade.php
+++ resources/views/administer/admin_employee.blade.php
@@ -1,9 +1,21 @@
 @extends('/administer/admin_template')
 
-@section('css','/admin/stock')
-@section('title','stock')
+@section('css','/admin/employee')
+@section('title','従業員リスト')
 
 @section('main')
+  <table class="table">
+    <tr>
+      <th>従業員ID</th>
+      <th>従業員名</th>
+    </tr>
+    @foreach ($employee as $key => $value)
+      <tr>
+        <td>{{$value->employee_id}}</td>
+        <td>{{$value->name}}</td>
+      </tr>
+    @endforeach
 
+  </table>
 
 @endsection
