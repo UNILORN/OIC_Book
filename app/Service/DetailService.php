0c23f960957e58fd37cb1ba03c00868ea385a171
yuyutyanu <yuyutyanu@yahoo.co.jp>
2016-11-29 03:06:13 +0900

    serviceディレクトリのリファクタリング

--- app/Service/DetailService.php
+++ app/Service/DetailService.php
@@ -6,8 +6,8 @@ use \App\PRODUCT;
 
 class DetailService{
   public function getDetail($product_id){
-    $detail = PRODUCT::where('product_id',$product_id)
-                      ->first();
+    
+    $detail = PRODUCT::where('product_id',$product_id)->first();
     return $detail;
   }
 }
