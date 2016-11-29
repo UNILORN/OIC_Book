9df97bdf48cfe22a140ce389cfad3a8f723c75be
UNILORN <b5159@oic.jp>
2016-11-29 12:06:44 +0900

    レビュー修正

--- database/seeds/REVIEW_TABLE.php
+++ database/seeds/REVIEW_TABLE.php
@@ -17,13 +17,15 @@ class REVIEW_TABLE extends Seeder
         //faker使う。普通に使う場合と同じ。
         $faker = Faker\Factory::create('ja_JP');
 
+        $exp = ["面白かった","とても良かった","良かった","微妙"];
+
         for ($i = 1; $i <= 100; $i++) {
             for ($j = 1; $j <= 10; $j++) {
                 DB::table('REVIEW')->insert([
                     'product_id' => $i,
                     'user_id' => $j,
                     'review' => $faker->randomDigitNotNull(),
-                    'review_text' => $faker->word(),
+                    'review_text' => $exp[ $j % 4 ],
                     'entry_time' => $faker->dateTimeThisCentury()
                 ]);
             }
