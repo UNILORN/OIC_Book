02368bc812fc6648c3fcd7bb023293c0edb33874
UNILORN <b5159@oic.jp>
2016-12-02 14:08:27 +0900

    発注情報を更新

--- app/Http/Controllers/AdminarriveController.php
+++ app/Http/Controllers/AdminarriveController.php
@@ -11,19 +11,28 @@ use App\ARRIVE;
 
 class AdminarriveController extends BaseController
 {
-  public function index(Request $request){
-    $arrive = ARRIVE::ID($request->input('arrive_id'))
-        ->OrderID($request->input('order_id'))
-        ->PlanFrom($request->input('arrive_plan_from'))
-        ->PlanTo($request->input('arrive_plan_to'))
-        ->DayFrom($request->input('arrive_day_from'))
-        ->DayTo($request->input('arrive_day_to'))
-        ->with('arriveOrder')
-        ->with('arriveOrder.orderProduct')
-        ->with('arriveEmployee')
-        ->paginate(20);
+
+    public function index(Request $request)
+    {
+        $arrive = ARRIVE::ID($request->input('arrive_id'))
+            ->OrderID($request->input('order_id'))
+            ->PlanFrom($request->input('arrive_plan_from'))
+            ->PlanTo($request->input('arrive_plan_to'))
+            ->DayFrom($request->input('arrive_day_from'))
+            ->DayTo($request->input('arrive_day_to'))
+            ->with('arriveOrder')
+            ->with('arriveOrder.orderProduct')
+            ->with('arriveEmployee')
+            ->paginate(20);
 
 
-    return view('/administer/arrive/admin_arrive',compact('arrive','request'));
-  }
+        return view('/administer/arrive/admin_arrive', compact('arrive', 'request'));
+    }
+
+    public function create()
+    {
+
+
+    }
+
 }
