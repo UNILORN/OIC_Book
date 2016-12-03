673cac24aca50a15fc2cd22ec8483862f1cfdafd
yuyutyanu <yuyutyanu@yahoo.co.jp>
2016-12-03 11:54:48 +0900

    追加する商品がすでにカートにあった場合の処理追加(sessionカート)

--- app/Service/SessioncartService.php
+++ app/Service/SessioncartService.php
@@ -5,49 +5,58 @@ use App\PRODUCT;
 
 class SessioncartService{
 
-  public function addProduct($product_id,$number){
+  public function addProduct($product_id,$number)
+  {
+    $insert_product =  PRODUCT::where('product_id',$product_id)->first();
+    $insert_product["number"] = $number;
+    $current_cart = session()->get("cart",[]);
 
-    $product =  PRODUCT::where('product_id',$product_id)->first();
-    $product["number"] = $number;
-    $cart = session()->get("cart",[]);
-    $cart[] = $product;
+    foreach ($current_cart as $current_cart)
+    {
+      if($insert_product->product_id === $current_cart->product_id)
+      {
+          $products = session()->get("cart",[]);
+          return $products;
+      }
+    }
 
+    $cart[] = $insert_product;
     session()->put("cart",$cart);
     $products = session()->get("cart",[]);
 
     return $products;
   }
 
-  public function getTotalfee(){
-
+  public function getTotalfee()
+  {
     $products = session()->get("cart",[]);
     $sum = 0;
-    foreach ($products as $product) {
+    foreach ($products as $product)
+    {
       $sum += $product->price;
     }
 
     return $sum;
   }
 
-  public function getItems(){
-
+  public function getItems()
+  {
     $products = session()->get("cart",[]);
-
     return $products;
   }
 
-  public function deleteItem($index){
-
+  public function deleteItem($index)
+  {
     session()->forget("cart.$index");
     $products = session()->get("cart",[]);
 
     return $products;
   }
 
-  public function changeQuantity($index){
-  }
+  public function changeQuantity($index){}
 
-  public function allDelete(){
+  public function allDelete()
+  {
     session()->flush();
   }
 }
