1baf788953bd6fbff755f0f0a927ead08f3740f4
UNILORN <b5159@oic.jp>
2016-12-03 19:04:47 +0900

    発注シード変更

--- database/seeds/ORDER_TABLE.php
+++ database/seeds/ORDER_TABLE.php
@@ -23,7 +23,7 @@ class ORDER_TABLE extends Seeder
             'vender_id'=>$i,
             'employee_id'=>$i%50,
             'product_id'=>$i,
-            'order_number'=>$faker->randomDigitNotNull(),
+            'order_number'=>9,
             'order_day'=>$faker->dateTimeThisCentury(),
             'arrive_plan'=>$faker->dateTimeThisCentury(),
             'remaining_amount'=>$faker->randomDigitNotNull(),
