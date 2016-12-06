eef5881938841a07233d8ceb1e6b606b6b3d23e7
Unilorn <b5159@oic.jp>
2016-12-06 15:50:31 +0900

    管理者画面　仕入先情報追加

--- database/seeds/VENDOR_TABLE.php
+++ database/seeds/VENDOR_TABLE.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\VENDOR;
+
 class VENDOR_TABLE extends Seeder
 {
     /**
@@ -11,20 +12,19 @@ class VENDOR_TABLE extends Seeder
      */
     public function run()
     {
-      DB::table('VENDOR')->delete();
+        DB::table('VENDOR')->delete();
 
-       //faker使う。普通に使う場合と同じ。
-      $faker = Faker\Factory::create('ja_JP');
+        //faker使う。普通に使う場合と同じ。
+        $faker = Faker\Factory::create('ja_JP');
 
-      for($i=1; $i<=100; $i++)
-      {
-          DB::table('VENDOR')->insert([
-            'vendor_id'=>$i,
-            'vendor_name'=>$faker->company(),
-            'vendor_email'=>$faker->email(),
-            'vendor_address'=>$faker->address(),
-            'vendor_phone_number'=>$faker->phoneNumber()
-          ]);
-      }
+        for ($i = 1; $i <= 100; $i++) {
+            DB::table('VENDOR')->insert([
+                'vendor_id' => $i,
+                'vendor_name' => $faker->company(),
+                'vendor_email' => $faker->email(),
+                'vendor_address' => $faker->address(),
+                'vendor_phone_number' => $faker->phoneNumber()
+            ]);
+        }
     }
 }
