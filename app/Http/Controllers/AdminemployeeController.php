92aec5f534d4a080f98752847096946b3c09594a
Unilorn <b5159@oic.jp>
2016-12-13 12:13:08 +0900

    従業員情報　seed等変更

--- app/Http/Controllers/AdminemployeeController.php
+++ app/Http/Controllers/AdminemployeeController.php
@@ -8,14 +8,20 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use App\User;
+use App\EMPLOYEE;
 
 class AdminemployeeController extends BaseController
 {
-  public function index(Request $request){
-    $employee = User::whereNotNull('employee_id')
-    ->EmployeeID($request->input('employee_id'))
-    ->Name($request->input('employee_name'))
-    ->paginate(20);
-    return view('/administer/employee/admin_employee',compact('employee','request'));
-  }
+    public function index(Request $request)
+    {
+        $employee = User::whereNotNull('employee_id')
+            ->EmployeeID($request->input('employee_id'))
+            ->Name($request->input('employee_name'))
+            ->paginate(20);
+
+        $employee = EMPLOYEE::active()
+            ->paginate(20);
+
+        return view('/administer/employee/admin_employee', compact('employee', 'request'));
+    }
 }
