efb2c8bd58edd6914961e9f4e4d0f6e54ed8919b
Unilorn <b5159@oic.jp>
2016-12-13 14:49:25 +0900

    従業員スコープ追加

--- app/EMPLOYEE.php
+++ app/EMPLOYEE.php
@@ -10,18 +10,35 @@ class EMPLOYEE extends Model
     protected $table = 'EMPLOYEE';
     protected $primaryKey = 'employee_id';
 
-    public function auth(){
-      return $this->hasMany('App\AUTH','auth_id','employee_auth_id');
-    }
-
-    public function user(){
-        return $this->belongsTo('App\User','employee_id');
-    }
-
-
-    public function scopeactive($query){
-        $query = $query->where('delete_flg',0);
+    public function scopeactive($query)
+    {
+        $query = $query->where('delete_flg', 0);
         return $query;
     }
+
+    public function scopeID($query, $id)
+    {
+        if (!isset($id)) {
+            $query = $query->where('employee_id', $id);
+        }
+        return $query;
+    }
+
+    public function scopeName($query, $name)
+    {
+        if (!isset($name)) {
+            $query = $query->where('employee_name','like', "%$name%");
+        }
+        return $query;
+    }
+
+    public function scopeEmail($query, $email)
+    {
+        if (!isset($email)) {
+            $query = $query->where('employee_email','like', "%$email%");
+        }
+        return $query;
+    }
+
 
 }
