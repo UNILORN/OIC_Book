2a2c42eeaad7ba7b5ee727ac6b046a27fe6aa73c
Unilorn <b5159@oic.jp>
2016-12-16 01:00:22 +0900

    カートに商品がないと購入画面へ進めなくなりました。

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests;
+use App\Service\AuthcartService;
 
 class BuyController extends Controller
 {
-  public function index(){
+    public function index()
+    {
 
-    if (Auth::check()) {
-      return view('buy');
-    }else{
-      return view('auth/login');
-    }
+
+        if (Auth::check()) {
+            $cart = new AuthcartService;
+            if(!empty($cart->getItems(Auth::id())->toArray())){
+                return view('buy');
+            }
+            return redirect('/');
+        } else {
+            return view('auth/login');
+        }
     }
 }
