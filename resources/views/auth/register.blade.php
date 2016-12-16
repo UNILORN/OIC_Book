c9203c13f5c0b8ca042242063999b8f7156ed5ad
chiii <b5002@oic.jp>
2016-12-16 12:20:16 +0900

    ログイン、新規登録画面css変更

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,157 +1,47 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal formfadeout" role="form" method="POST" action="{{ url('/register') }}">
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+  </head>
+  <body>
+    <form action="/register" method="post" class="center-block">
                         {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" v-model="name" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" v-model="email"class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                          <label for="user_post_code" class="col-md-4 control-label">郵便番号</label>
-
-                          <div class="col-md-6">
-                            <input id="user_post_code" type="text" v-model="user_post_code" class="form-control" name="user_post_code" required>
+                        <div class="form">
+                          <div class="form-item">
+                            <label for="name"></label>
+                            <input type="text" name="name" required="required" placeholder="Your Name"></input>
                           </div>
-                        </div>
-
-                        <div class="form-group">
-                          <label for="user_address" class="col-md-4 control-label">住所</label>
-
-                          <div class="col-md-6">
-                            <input id="user_address" type="text" class="form-control" v-model="user_address" name="user_address" required>
+                          <div class="form-item">
+                            <label for="email"></label>
+                            <input type="email" name="email" required="required" placeholder="Email Address"></input>
                           </div>
-                        </div>
-
-                        <div class="form-group">
-                          <label for="user_phone_number" class="col-md-4 control-label">電話番号</label>
-
-                          <div class="col-md-6">
-                            <input id="user_phone_number" type="text" class="form-control" v-model="user_phone_number" name="user_phone_number" required>
+                          <div class="form-item">
+                            <label for="password"></label>
+                            <input type="password" name="password" required="required" placeholder="Password"></input>
                           </div>
-                        </div>
+                          <div class="form-item">
+                            <label for="password"></label>
+                            <input type="password" name="password_confirmation" required="required" placeholder="Confirmation Password"></input>
+                          </div>
+                          <div class="form-item">
+                            <label for="user_post_code"></label>
+                            <input type="text" name="user_post_code" required="required" placeholder="〒"></input>
+                          </div>
+                          <div class="form-item">
+                            <label for="user_address"></label>
+                            <input type="text" name="user_address" required="required" placeholder="Your Address"></input>
+                          </div>
+                          <div class="form-item">
+                            <label for="user_phone_number"></label>
+                            <input type="tel" name="user_phone_number" required="required" placeholder="Tel"></input>
+                          </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
 
 
-                                      <button  style="display:none" type="submit" class="btn btn-primary submitbutton">
-                                      Register
-                                      </button>
-                                      <button type="button" class="confirm_button">確認</button>
-                                      <button type="button" style="display:none" class="confirm_button2">戻る</button>
-
-                                </div>
-                        </div>
+                        <input class="btn" type="submit" value="確認">
+                      </div>
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="confirm" style="display:none">
-  <div class="confirm_text">
-    <p>name:@{{name}}</p>
-    <p>email:@{{email}}</p>
-    <p>郵便番号:@{{user_post_code}}</p>
-    <p>住所:@{{user_address}}</p>
-    <p>電話番号:@{{user_phone_number}}</p>
-  </div>
-</div>
-
-    <script type="text/javascript">
-    new Vue({
-      e1:'body',
-      date:{
-        name:'name',
-        email:'email',
-        user_post_code:'user_post_code',
-        user_address:'user_address',
-        user_phone_number:'user_phone_number'
-      }
-
-    })
-    </script>
-<style media="screen">
-  .confirm{
-    position: fixed;
-    top: 100px;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height:350px;
-    width:700px;
-    background: rgba(1, 1, 1, 0.8);
-    margin: 0 auto;
-  }
-  .confirm_text{
-    text-align: center;
-
-  }
-</style>
-<script type="text/javascript">
-$('.confirm_button').click(function(){
-  $('.submitbutton').fadeIn();
-  $('.confirm').fadeIn();
-  $('.confirm_button2').fadeIn();
-})
-$('.confirm_button2').click(function(){
-  $('.submitbutton').fadeOut();
-  $('.confirm').fadeOut();
-  $('.confirm_button2').fadeOut();
-})
-
-</script>
-@endsection
+                  </body>
+                </html>
