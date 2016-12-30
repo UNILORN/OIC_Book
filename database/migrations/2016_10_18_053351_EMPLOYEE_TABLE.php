2c30dd26667747820f489e82644d9f0fe36f9f97
sizukutamago <b5241@oic.jp>
2016-12-30 19:59:07 +0900

    EMPLOYEE_TABLEに権限を追加(default値も追加)

--- database/migrations/2016_10_18_053351_EMPLOYEE_TABLE.php
+++ database/migrations/2016_10_18_053351_EMPLOYEE_TABLE.php
@@ -18,7 +18,7 @@ class EMPLOYEETABLE extends Migration
             $table->string('employee_email')->unique();
             $table->string('employee_name');
             $table->string('employee_password', 255);
-            $table->integer('employee_authority');//従業員の権限 1:売り上げが見れない 2:売り上げが観れる 3:従業員の新規登録とか変更とか
+            $table->integer('employee_authority')->default(0);//従業員の権限 1:売り上げが見れない 2:売り上げが観れる 3:従業員の新規登録とか変更とか
             $table->string('employee_phone_number');
             $table->boolean('delete_flg')->default(0);
             $table->timestamps();
