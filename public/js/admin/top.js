1ab42d0a2656fe5c8ae8cb067bf38ee2c180caee
sizukutamago <b5241@oic.jp>
2017-01-20 11:09:18 +0900

    topjs

--- public/js/admin/top.js
+++ public/js/admin/top.js
@@ -60,7 +60,7 @@ $(function () {
             var date = new Date();
             date.setMonth(date.getMonth());
             monthname = date.getMonth();
-            salesdata[salesdata.length] = [monthname + "月", monthlysales[key], ""];
+            salesdata[salesdata.length] = [1 + "月", monthlysales[key], ""];
         }
 
         google.charts.load('current', {packages: ['corechart', 'bar']});
