1fed46afdadf51de5b9c81be362d6a215d4194e9
sizukutamago <b5241@oic.jp>
2017-01-24 16:02:28 +0900

    admin/topの1をmonthnameに変更(元通りに)

--- public/js/admin/top.js
+++ public/js/admin/top.js
@@ -62,7 +62,7 @@ $(function () {
             date.setMonth(date.getMonth() - d--);
             monthname = date.getMonth() + 1;
 
-            salesdata[salesdata.length] = [1 + "月", monthlysales[key], ""];
+            salesdata[salesdata.length] = [monthname + "月", monthlysales[key], ""];
         }
 
         google.charts.load('current', {packages: ['corechart', 'bar']});
